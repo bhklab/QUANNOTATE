51c4b72e91fe4ed39d03cc9eb411d61dc3a85e68
deventorum <deventorum@gmail.com>
2020-09-17 13:57:25 -0400

    Rearranged architecture

--- app.js
+++ app.js
@@ -2,15 +2,12 @@ require('dotenv').config();
 
 // const join = require('path').join;
 const express = require('express');
-const db = require('./config/mongoose');
+const db = require('./database/mongoose');
 // const models = join(__dirname, 'app/models');
 
 const port = process.env.PORT || 5000;
 const app = express();
-
-// Bootstrap routes
-// require('./config/express')(app);
-// require('./config/routes')(app);
+require('./config/express')(app);
 
 db.connection
 	.on('error', console.log)
